41ca2c28157eff5c47326ab8c8c7f8318434944c
fortu038 <fortu038@umn.edu>
2022-10-25 22:28:28 -0500

    created deletes for all routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -45,13 +45,30 @@ router.post('/', async (req, res) => {
   try {
     const tagData = await Tag.create(req.body);
     res.status(200).json(tagData);
-  } catch (err) {
+  } 
+  catch (err) {
     res.status(400).json(err);
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const tagData = await Tag.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if(!tagData) {
+      res.status(404).json({message: 'No location found with this id!'});
+    }
+
+    res.status(200).json(tagData);
+  }
+  catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', async (req, res) => {
@@ -63,13 +80,14 @@ router.delete('/:id', async (req, res) => {
       }
     });
 
-    if (!tagData) {
-      res.status(404).json({ message: 'No location found with this id!' });
+    if(!tagData) {
+      res.status(404).json({message: 'No location found with this id!'});
       return;
     }
 
     res.status(200).json(tagData);
-  } catch (err) {
+  } 
+  catch (err) {
     res.status(500).json(err);
   }
 });
